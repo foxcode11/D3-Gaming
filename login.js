d3a9e2688ce99b6c0e1154aa8579587c2ac4c296
Deepak Patel <foxcode11@gmail.com>
2023-03-03 19:39:47 +0530

    wedsite

---
+++ login.js
@@ -0,0 +1,82 @@
+const mysql = require("mysql2");
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+const encoder = bodyParser.urlencoded();
+
+const app = express();
+app.use("/public/assets",express.static("assets"));
+app.use(express.static("public"));
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "@Blackfox11",
+//     enter your mysql password
+    database: "nodejs"
+});
+
+// connect to the database
+connection.connect(function(error){
+    if (error) throw error
+    else console.log("connected to the database successfully!")
+});
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+
+app.get("/contactUs",function(req,res){
+    res.sendFile(__dirname + "/contactUs.html");
+})
+
+app.get("/signup",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+})
+app.get("/login",function(req,res){
+    res.sendFile(__dirname + "/login.html");
+})
+
+app.post("/login",encoder, function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    connection.query("select * from loginuser where user_name = ? and user_pass = ?",[username,password],function(error,results,fields){
+    
+        if (results.length > 0) {
+            
+            res.redirect("/");
+        } else { 
+            res.redirect("/");
+        }
+        res.end();
+    })
+})
+
+app.post("signup.html",encoder, function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+   
+    connection.connect(function(err) {
+        console.log("Connected!");
+        var sql = "INSERT INTO loginuser (user_name, user_pass) VALUES (?,?);";
+        connection.query(sql, [username,password],function (err, result) {
+            if(err)
+            console.log(err);
+            else
+          console.log("1 record inserted");
+        });
+      });
+res.redirect("login.html");
+})
+
+// when login is success
+app.get("/welcome",function(req,res){
+    res.sendFile(__dirname + "/welcome.html")
+})
+
+
+// set app port
+app.listen(3000);
